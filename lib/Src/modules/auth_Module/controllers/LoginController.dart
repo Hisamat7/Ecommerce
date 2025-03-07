4e88ed058709c84f2eec07644106e3f820697e94
Hisamat7 <muhammedhisam10@gmail.com>
2025-03-07 08:19:29 +0530

    new

--- lib/Src/modules/auth_Module/controllers/LoginController.dart
+++ lib/Src/modules/auth_Module/controllers/LoginController.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/Src/modules/auth_Module/views/SignIn.dart';
+import 'package:ecommerce/Src/modules/auth_Module/views/SignUpProcess.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,6 +12,8 @@ class LoginController extends GetxController {
       String? userName}) async {
     try {
    final credential  =   await FirebaseAuth.instance .createUserWithEmailAndPassword(email: email, password: password);
+   Get.off(() => SignIn());
+   FirebaseFirestore.instance.collection("users").doc(credential.user!.uid).set({"userName":userName,"userEmail":email});
     }on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         SnackBar(content: Text('The password provided is too weak.'));
@@ -21,6 +26,25 @@ class LoginController extends GetxController {
       AlertDialog(
         content: Text(e.toString()),
       );
+    }
+  }
+  loginAccount({required String email,required String password})async{
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      Get.off(() => SignUpProcess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        SnackBar(content: Text('No user found for that email.'));
+      } else if (e.code == 'wrong-password') {
+        SnackBar(content: Text('Wrong password provided for that user.'));
+      }
+    }
+    catch (e) {
+      AlertDialog(
+        content: Text(e.toString()),
+      
+      );
+      
     }
   }
 }
--- lib/Src/modules/auth_Module/views/SignIn.dart
+++ lib/Src/modules/auth_Module/views/SignIn.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/Src/const/App_Colors.dart';
 import 'package:ecommerce/Src/const/App_Images.dart';
 import 'package:ecommerce/Src/const/Button.dart';
+import 'package:ecommerce/Src/modules/auth_Module/controllers/LoginController.dart';
 import 'package:ecommerce/Src/modules/auth_Module/views/SignUp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,6 +15,10 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> loginKey = GlobalKey<FormState>();
+  LoginController loginController = Get.find<LoginController>();
   Widget googleAndFacebook({required String text, required String image}) {
     return Container(
       height: 50.h,
@@ -39,97 +44,102 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 310.h,
-              width: double.infinity,
-              child: Stack(
-                children: [
-                  Image.asset(
-                    AppImages.background,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                  Positioned(
-                    top: 0.h,
-                    left: 0.w,
-                    right: 0.w,
-                    bottom: 0.h,
-                    child: Image.asset(
-                      AppImages.logo,
+      body: Form(
+        key: loginKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300.h,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      AppImages.background,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
                     ),
-                  ),
-                ],
+                    Positioned(
+                      top: 0.h,
+                      left: 0.w,
+                      right: 0.w,
+                      bottom: 0.h,
+                      child: Image.asset(
+                        AppImages.logo,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 60.h,
-                  ),
-                  TextFormField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
-                        labelText: "Email",
-                        hintStyle: TextStyle(
-                            color: const Color.fromARGB(255, 103, 102, 102)),
-                        hintText: "Email",
-                        fillColor: Color(0xFF252525),
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none)),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  TextFormField(
-                    style: TextStyle(color: Colors.white),
-                    obscureText: obscureText,
-                    decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
-                        suffixIcon: IconButton(
-                          icon: Icon(obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              obscureText = !obscureText;
-                            });
-                          },
-                        ),
-                        labelText: "Password",
-                        hintStyle: TextStyle(
-                            color: const Color.fromARGB(255, 103, 102, 102)),
-                        hintText: "Password",
-                        fillColor: Color(0xFF252525),
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none)),
-                  ),
-                ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 60.h,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      validator: (value) => value!.isEmpty ? "Enter email" : null,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          labelStyle: TextStyle(color: Colors.white),
+                          labelText: "Email",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 103, 102, 102)),
+                          hintText: "Email",
+                          fillColor: Color(0xFF252525),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none)),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      validator: (value) => value!.isEmpty ? "Enter password" : null,
+                      style: TextStyle(color: Colors.white),
+                      obscureText: obscureText,
+                      decoration: InputDecoration(
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: IconButton(
+                            icon: Icon(obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                obscureText = !obscureText;
+                              });
+                            },
+                          ),
+                          labelText: "Password",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 103, 102, 102)),
+                          hintText: "Password",
+                          fillColor: Color(0xFF252525),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none)),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Center(
-              child: Text(
-                "Or continue with",
-                style: TextStyle(color: Colors.white),
+              SizedBox(
+                height: 20.h,
               ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Container(
-              child: Row(
+              Center(
+                child: Text(
+                  "Or continue with",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   googleAndFacebook(
@@ -138,48 +148,52 @@ class _SignInState extends State<SignIn> {
                       text: "Google", image: AppImages.googleicon),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            Center(
-              child: Text(
-                "Forgot Your Password?",
-                style: TextStyle(color: buttonColor),
+              SizedBox(
+                height: 25.h,
               ),
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            Button(
-              onPressed: () {},
-              text: "Login",
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account?",
-                  style: TextStyle(color: Colors.white),
+              Center(
+                child: Text(
+                  "Forgot Your Password?",
+                  style: TextStyle(color: buttonColor),
                 ),
-                SizedBox(
-                  width: 5.w,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Get.to(()=>SignUp());
-                  },
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(color: buttonColor),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Button(
+                onPressed: () {
+                  if(loginKey.currentState!.validate()){
+                    loginController.loginAccount(email: emailController.text, password: passwordController.text);
+                  }
+                },
+                text: "Login",
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(color: Colors.white),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 5.w,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.to(()=>SignUp());
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: buttonColor),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
