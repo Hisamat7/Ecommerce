78bf51ebb64caba034dad88bb641d767de23a1c5
Hisamat7 <muhammedhisam10@gmail.com>
2025-03-08 19:52:04 +0530

    oky

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:get/get.dart';
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
